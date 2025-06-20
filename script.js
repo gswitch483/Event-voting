90d2b052d3b1d4addc79327b23b1edec34ed642d
gswitch483 <gswitch483@users.noreply.github.com>
2025-06-20 22:48:12 +0000

    deploy: 1d57306ffbf27c35012569bb73cce0a3568c5b2b

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Firebase configuration and voting logic
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getDatabase, ref, push, get, set, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtYV2z9q2sCwn6qKceSVWJ7Rp1Rd8h9ls",
+    authDomain: "eventvotingapp-4cb94.firebaseapp.com",
+    databaseURL: "https://eventvotingapp-4cb94-default-rtdb.firebaseio.com",
+    projectId: "eventvotingapp-4cb94",
+    storageBucket: "eventvotingapp-4cb94.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abc123def456ghi789"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Remainder of your voting logic here...
